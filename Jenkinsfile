0760104410212dc61dbf6725fb1b67b017a67a3d
suryakalyan403@gmail.com <kalyansurya403@gmail.com>
2025-02-20 06:18:51 -0500

    Added the Jenkinsfile and added the Env variable to the pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,4 @@
+@Library("com.i27academy.slb@main") _
+firstpipeline(
+    ENV_NAME: 'Development'  
+)
---
+++ vars/RafmPipeline.groovy
@@ -0,0 +1,31 @@
+def call(Map pipelineparams) {
+
+    RafmDeployments rafmdeployments = new RafmDeployments()
+
+
+    pipeline {
+      agents any
+
+      stages{
+
+        stage("Extract Docker Image") {
+
+           steps {
+
+            script {
+                
+                echo "===================${pipelineparams.ENV-NAME}============"
+                echo "Method Call: ${rafmdeployments.dockerImgExtract('/home/rsoni/base/0.4.9/images', 'base-0.4.9.tar.gz') }"
+
+              }
+
+            }
+
+         }
+
+      }
+
+   }
+
+ }
+
